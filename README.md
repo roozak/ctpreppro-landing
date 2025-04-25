6e6810fd7f19f44d7971fbc48f630ef51587e962
roozak <roozak@yahoo.com>
2025-04-25 01:03:42 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# CTPrepPro Landing Page
+
+This is a simple React-based pre-launch email capture page for CTPrepPro.
---
+++ index.js
@@ -0,0 +1,8 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import LandingPage from './LandingPage';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<LandingPage />);
